355446e97d2bc89e3226fa3ebc062d0e843a9aa6
Yevhenii Kalashnikov <lumyk@me.com>
2024-06-12 18:55:38 +0100

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,43 @@
+// swift-tools-version:5.5
+import PackageDescription
+
+let package = Package(
+    name: "tippecanoe-swift",
+    platforms: [
+        .iOS(.v13)
+    ],
+    products: [
+        .library(
+            name: "tippecanoe-swift",
+            targets: ["tippecanoe-swift"]
+        ),
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "tippecanoe-swift",
+            dependencies: [],
+            path: "Sources",
+            exclude: ["../install.sh"],
+            sources: [
+                "tippecanoe/catch",
+                "tippecanoe/jsonpull",
+                "tippecanoe/milo",
+                "tippecanoe/protozero",
+                "tippecanoe/mapbox"
+            ],
+            publicHeadersPath: "tippecanoe",
+            cSettings: [
+                .headerSearchPath("tippecanoe"),
+                .define("TARGET_OS_IPHONE", to: "1"),
+                .unsafeFlags(["-O3", "-w"])
+            ],
+            linkerSettings: [
+                .linkedLibrary("sqlite3"),
+                .linkedLibrary("z"),
+                .linkedLibrary("c++")
+            ]
+        )
+    ],
+    swiftLanguageVersions: [.v5]
+)
